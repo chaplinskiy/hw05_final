98e722fbcef1978617341cb3652581947879a882
Ivan Chaplinskiy <chaplinskiy@ya.ru>
2021-07-10 14:26:33 +0300

    1st review: fixed accordingly

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from django.db import models
-from django.db.models import constraints
 from django.utils.text import slugify
 
 User = get_user_model()
